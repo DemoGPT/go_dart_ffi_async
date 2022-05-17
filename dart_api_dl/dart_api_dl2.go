f79c4e5d2cab2b76a519ed9b8abdd19e5e5e1bce
tait <tait.elian@outlook.com>
2022-05-17 22:40:51 +0800

    no message

--- dart_api_dl/dart_api_dl2.go
+++ dart_api_dl/dart_api_dl2.go
@@ -1,7 +1,6 @@
 package dart_api_dl
 
 // #include "stdint.h"
-// #include "stdlib.h"
 // #include "include/dart_api_dl.c"
 //
 // // Go does not allow calling C function pointers directly. So we are
@@ -9,21 +8,6 @@ package dart_api_dl
 // bool GoDart_PostCObject(Dart_Port_DL port, Dart_CObject* obj) {
 //   return Dart_PostCObject_DL(port, obj);
 // }
-//
-//	typedef struct WorkStruct{
-//		char *str;
-//	}WorkStruct;
-//
-//	int64_t GetWork(void **ppWork, char* str) {
-//		WorkStruct *pWork = (WorkStruct *)malloc(sizeof(WorkStruct));
-//		pWork->str=str;
-//
-//		*ppWork = pWork;
-//
-//		int64_t ptr = (int64_t)pWork;
-//
-//		return ptr;
-//	}
 import "C"
 import "unsafe"
 
@@ -34,13 +18,10 @@ func Init(api unsafe.Pointer) {
 	}
 }
 
-func SendToPort(port int64, msg string) {
-    var obj C.Dart_CObject
+func SendToPort(port int64, msg int64) {
+	var obj C.Dart_CObject
 	obj._type = C.Dart_CObject_kInt64
-
-	var pWork unsafe.Pointer
-	ptrAddress := C.GetWork(&pWork, C.CString(msg))
-
-	*(*C.int64_t)(unsafe.Pointer(&obj.value[0])) = ptrAddress
-	C.GoDart_PostCObject(C.int64_t(port), &obj)
+	// cgo does not support unions so we are forced to do this
+	*(*C.int64_t)(unsafe.Pointer(&obj.value[0])) = C.int64_t(msg)
+    C.GoDart_PostCObject(C.int64_t(port), &obj)
 }
